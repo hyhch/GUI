acd3470d658014a86bd6f5b68144c6a300cbfd77
hyhch <71998203+hyhch@users.noreply.github.com>
2022-08-01 16:42:02 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -40,6 +40,7 @@ db.transaction(function (context) {
    context.executeSql('CREATE TABLE IF NOT EXISTS TemplateList ("id" INTEGER NOT NULL,"name" TEXT,PRIMARY KEY ("id"))');
    context.executeSql('CREATE TABLE IF NOT EXISTS Button ("id" INTEGER NOT NULL,"name" TEXT,"hwid" TEXT,"areaId" TEXT,"templateId" INTEGER,PRIMARY KEY ("id"))');
    context.executeSql('CREATE TABLE IF NOT EXISTS LED ("id" INTEGER NOT NULL,"name" TEXT,"hwid" TEXT,"areaId" TEXT,"templateId" INTEGER,PRIMARY KEY ("id"))');
+   context.executeSql('CREATE TABLE IF NOT EXISTS ButtonMapping ("id" INTEGER NOT NULL,"name" TEXT,"unitname" TEXT,"buttonMode" TEXT,"templateId" INTEGER,"buttonId" INTEGER,PRIMARY KEY ("id"))');
    //创建表
   //  context.executeSql('INSERT INTO Button (id, name) VALUES (1, "aaa")');
   //  context.executeSql('INSERT INTO Button (id, name) VALUES (2, "aaa")');
