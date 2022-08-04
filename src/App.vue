ef1035d761f9bf4c2ef47b1db9fb212766cf91ef
hyhch <71998203+hyhch@users.noreply.github.com>
2022-08-04 14:04:07 +0800

    Update App.vue
    1、将页面初始化方法写入initPage函数并在mounted中调用
    2、router-view绑定route的fullpath，path中参数有改变会自动改变页面

--- src/App.vue
+++ src/App.vue
@@ -3,37 +3,18 @@
     
     
     <el-col :span="22" style="overflow-x:auto">
-      <router-view />
+      <router-view :key="$route.fullPath" />
     </el-col>
 
     <el-col :span="2">
       <div v-for="(t_item, index) in templates" :key="index">
-        <router-link :to="t_item.path" @click="createDB()">{{t_item.name}}</router-link>
+        <router-link :to="t_item.path">{{t_item.name}}</router-link>
       </div>
     </el-col>
   </el-row>
 </template>
 
 <script>
-  //元素唯一id
-  var db = openDatabase('BSHdb', '1.0', 'Test DB', 2 * 1024 * 1024);
-  db.transaction(function (context) {  
-
-    //  tx.executeSql('CREATE TABLE IF NOT EXISTS Temp ("id" INTEGER NOT NULL,"type" INTEGER,"areaId" INTEGER,"elementId" INTEGER,PRIMARY KEY ("id"))');
-   context.executeSql('CREATE TABLE IF NOT EXISTS TemplateList ("id" INTEGER NOT NULL,"name" TEXT,PRIMARY KEY ("id"))');
-   context.executeSql('CREATE TABLE IF NOT EXISTS Button ("id" INTEGER NOT NULL,"name" TEXT,"hwId" TEXT,"areaId" TEXT,"templateId" INTEGER,"boardType" INTEGER,PRIMARY KEY ("id"))');
-   context.executeSql('CREATE TABLE IF NOT EXISTS LED ("id" INTEGER NOT NULL,"name" TEXT,"hwId" TEXT,"areaId" TEXT,"segmentId" INTEGER,"templateId" INTEGER,"boardType" INTEGER,PRIMARY KEY ("id"))');
-   context.executeSql('CREATE TABLE IF NOT EXISTS ButtonMapping ("id" INTEGER NOT NULL,"name" TEXT,"unitname" TEXT,"buttonMode" TEXT,"templateId" INTEGER,"buttonId" INTEGER,PRIMARY KEY ("id"))');
-   context.executeSql('CREATE TABLE IF NOT EXISTS Segment ("id" INTEGER NOT NULL,"name" TEXT,"templateId" INTEGER,"boardType" INTEGER,PRIMARY KEY ("id"))');
-     //创建表
-    //  context.executeSql('INSERT INTO Button (id, name) VALUES (1, "aaa")');
-    //  context.executeSql('INSERT INTO Button (id, name) VALUES (2, "aaa")');
-    //  context.executeSql('INSERT INTO Button (id, name) VALUES (3, "aaa")');
-    //  context.executeSql('INSERT INTO LED (id, name) VALUES (1, "aaa")');
-    //  context.executeSql('INSERT INTO LED (id, name) VALUES (2, "aaa")');
-    
-  //取所有表中的id最大值+1作为elementCount值
-});
   export default {
     data() {
       return {
@@ -45,15 +26,43 @@
           },
           {
             name: "DEMO2",
-            path: "/templateDemo2"
+            path: "/templateDemo2/0"
           }
 
         ]
       }
     },
-
+    mounted:function(){
+      this.initPage();
+    },
     methods: {
-      
+      initPage(){
+      //元素唯一id
+        let db = openDatabase('BSHdb', '1.0', 'Test DB', 2 * 1024 * 1024);
+        let templates = this.templates;
+        db.transaction(function (context) {  
+          //  tx.executeSql('CREATE TABLE IF NOT EXISTS Temp ("id" INTEGER NOT NULL,"type" INTEGER,"areaId" INTEGER,"elementId" INTEGER,PRIMARY KEY ("id"))');
+          context.executeSql('CREATE TABLE IF NOT EXISTS TemplateList ("id" INTEGER NOT NULL,"name" TEXT,PRIMARY KEY ("id"))');
+          context.executeSql('CREATE TABLE IF NOT EXISTS Button ("id" INTEGER NOT NULL,"name" TEXT,"hwId" TEXT,"areaId" TEXT,"templateId" INTEGER,"boardType" INTEGER,PRIMARY KEY ("id"))');
+          context.executeSql('CREATE TABLE IF NOT EXISTS LED ("id" INTEGER NOT NULL,"name" TEXT,"hwId" TEXT,"areaId" TEXT,"segmentId" INTEGER,"templateId" INTEGER,"boardType" INTEGER,PRIMARY KEY ("id"))');
+          context.executeSql('CREATE TABLE IF NOT EXISTS ButtonMapping ("id" INTEGER NOT NULL,"name" TEXT,"unitname" TEXT,"buttonMode" TEXT,"templateId" INTEGER,"buttonId" INTEGER,PRIMARY KEY ("id"))');
+          context.executeSql('SELECT * FROM TemplateList',[],function(context,results){
+            let len = results.rows.length;
+            for(let i = 0;i<len;i++){
+              console.log(results);
+              templates.push({name:results.rows.item(i).name,path:"/templateDemo2/"+results.rows.item(i).id});
+            }
+          });
+     //创建表
+    //  context.executeSql('INSERT INTO Button (id, name) VALUES (1, "aaa")');
+    //  context.executeSql('INSERT INTO Button (id, name) VALUES (2, "aaa")');
+    //  context.executeSql('INSERT INTO Button (id, name) VALUES (3, "aaa")');
+    //  context.executeSql('INSERT INTO LED (id, name) VALUES (1, "aaa")');
+    //  context.executeSql('INSERT INTO LED (id, name) VALUES (2, "aaa")');
+    
+  //取所有表中的id最大值+1作为elementCount值
+});
+      }
     }
   }
 </script>
